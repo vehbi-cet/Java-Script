1d4897bd4ab5e9864088e044ecea67bdb8ae1005
Muhammed Vehbi Çetinkaya <119114919+vehbi-cet@users.noreply.github.com>
2023-04-16 16:14:49 +0300

    Simple-Adding

---
+++ JSAlgoritmalar-8/easy.js
@@ -0,0 +1,3 @@
+import { simpleAdding } from "./easy/8_simpleAdding.js";
+
+console.log(simpleAdding(5));
---
+++ JSAlgoritmalar-8/easy/8_simpleAdding.js
@@ -0,0 +1,14 @@
+export const simpleAdding = (num) =>{
+ 
+    let result = 0
+    for(let i=1; i<= num; i++){
+        result = result + i
+    }
+
+    return result
+}
+
+
+/*
+Verilecek olan tamsayıya kadar olan değerlerin toplanması(verilen sayı dahil)
+*/
