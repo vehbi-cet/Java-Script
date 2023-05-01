be362c9ea176f5a05d217e6984d934906c93de36
Muhammed Vehbi Çetinkaya <119114919+vehbi-cet@users.noreply.github.com>
2023-05-01 16:10:55 +0300

    Number-Check-Algorithm

---
+++ JSAlgoritmalar-11/easy.js
@@ -0,0 +1,3 @@
+import { checkNums } from "./easy/11_checkNums.js";
+
+console.log(checkNums(5,9))
---
+++ JSAlgoritmalar-11/easy/11_checkNums.js
@@ -0,0 +1,26 @@
+export const checkNums = (num1, num2) => {
+
+    if(num1 === num2){
+        return -1
+    }else if(num2 > num1){
+        return true
+    }else{
+        return false
+    }
+   
+}
+
+
+
+
+/*Using the JavaScript languge, have the  function CheckNums(num1,num2) take both
+parameters being passed and
+
+return the string true if num2 is greater then num1
+
+otherwise return the string false
+
+If the parameter values are equal to ecah other
+then return the string -1
+*/ 
+
