9af5d544fecd35fbed40d9d47414b5b24250689c
Muhammed Vehbi Çetinkaya <119114919+vehbi-cet@users.noreply.github.com>
2023-05-06 15:55:21 +0300

    Alphabet-Soup

---
+++ JSAlgoritmalar-13/easy.js
@@ -0,0 +1,3 @@
+import { alphabetSoup } from "./easy/13_alphabetSoup.js";
+
+console.log(alphabetSoup("eleynam"));
---
+++ JSAlgoritmalar-13/easy/13_alphabetSoup.js
@@ -0,0 +1,17 @@
+
+export const alphabetSoup = (str) => {
+
+    return str.split('').sort().join('')
+
+}
+
+
+/* 
+  Alphabet Soup
+Have the function AlphabetSoup(str) take the str string parameter
+being passed and return the string with the letters in 
+alphabetical order (ie. hello becomes ehllo).
+
+Assume numbers and punctuation symbols will not be
+included in the string.
+*/
