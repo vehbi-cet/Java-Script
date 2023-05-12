28c66998339fdac9ba42ae267f1b9e3490922978
Muhammed Vehbi Çetinkaya <119114919+vehbi-cet@users.noreply.github.com>
2023-05-12 21:30:56 +0300

    wordCount Algorithm

---
+++ JSAlgoritmalar-16/easy.js
@@ -0,0 +1,3 @@
+import { wordCout } from "./easy/16_wordCount.js";
+
+console.log(wordCout(" Never eat shredded wheat"))
---
+++ JSAlgoritmalar-16/easy/16_wordCount.js
@@ -0,0 +1,16 @@
+export const wordCout = (str) => {
+
+      return str.trim().split(' ').length
+
+
+}
+
+
+/* 
+Word Count
+Using the JavaScript languge, have the function WordCount(str)
+take the str string parameter being passed and return
+the number of words the string contains
+(ie. "Never eat shredded wheat" would return 4).
+Word will be separated by single spaces.
+*/
